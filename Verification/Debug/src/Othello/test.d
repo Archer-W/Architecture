03ac654fab315cc60b3111d89ab7c3b89e5d690b
Archer-W <wang.minmei1211@gmail.com>
2018-03-28 09:12:47 -0700

    wrong location

--- Verification/Debug/src/Othello/test.d
+++
@@ -1,8 +0,0 @@
-src/Othello/test.o: ../src/Othello/test.cpp ../src/Othello/othello.h \
- ../src/Othello/hash.h ../src/Othello/disjointset.h
-
-../src/Othello/othello.h:
-
-../src/Othello/hash.h:
-
-../src/Othello/disjointset.h:
